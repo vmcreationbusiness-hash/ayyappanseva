e1526e32167f914b44199140963a2e687f927b5e
Ayyappan Seva <ayyappanseva@gmail.com>
2026-04-03 16:52:20 +0530

    UI: Removed Global Voice Microphone from Navbar

--- app.js
+++ app.js
@@ -108,9 +108,6 @@ function renderNavbar() {
 
   // Always add Voice and Settings buttons
   actionsHtml += `
-    <button class="btn-menu voice-btn-global" onclick="startVoiceInput('name')" title="Voice Command">
-      🎤
-    </button>
     <button class="btn-menu settings-btn" onclick="toggleSettings()" title="Settings">
       ⚙️
     </button>
